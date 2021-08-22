59cf518b9dc57627d8c4c03993f15939f89070d6
Giorgi Kratsashvili <gkrat17@freeuni.edu.ge>
2021-08-23 00:38:40 +0400

    delete unnecessary code

--- Komarista/Shared/Components/ActivityIndicatorView.swift
+++ Komarista/Shared/Components/ActivityIndicatorView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ActivityIndicatorView: UIViewRepresentable {
     func makeUIView(context: UIViewRepresentableContext<ActivityIndicatorView>) -> UIActivityIndicatorView {
-        return UIActivityIndicatorView(style: .large)
+        UIActivityIndicatorView(style: .large)
     }
 
     func updateUIView(_ uiView: UIActivityIndicatorView, context: UIViewRepresentableContext<ActivityIndicatorView>) {
